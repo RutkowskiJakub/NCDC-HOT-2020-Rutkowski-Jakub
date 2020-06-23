6e4ea33da96e7968d0b99c7454ac3886a46c9557
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-06-24 00:53:29 +0200

    Dodano zadanie 1 i 2

---
+++ Galaktyka.java
@@ -0,0 +1,153 @@
+package galaktyka;
+
+
+/**
+ * @author Jakub Rutkowski
+ * 
+ * TO DO:
+ */
+public class Galaktyka {
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    public static void main(String[] args) {
+        
+        Integer NumberOfArguments = args.length;
+        boolean IsSizeValid = false;
+        boolean IsDirectionValid = false;
+        
+        if (NumberOfArguments == 1)
+        {
+            char tmp = args[0].charAt(args[0].length()-1);
+            String StartingDirection = String.valueOf(tmp);
+            String CurrentDirection = StartingDirection;
+            Integer GalacticSize = Integer.parseInt(args[0].substring(0, args[0].length() - 1));
+            
+            if (GalacticSize >= 1 && GalacticSize <= 10000) IsSizeValid = true;
+            if ("W" == StartingDirection.intern() || "E" == StartingDirection.intern() || "N" == StartingDirection.intern() || "S" == StartingDirection.intern()) IsDirectionValid = true;
+            
+            if (IsDirectionValid == true && IsSizeValid == true)
+            {
+                Integer HorizontalDirection = 0;
+                Integer VerticalDirection = 0;
+                String row="";
+                Integer SpiralLength = 3;
+
+                Integer NumberOfRows=1;
+                Integer NumberOfColumns=1;
+                Integer HorizontalPointer=0;
+                Integer VerticalPointer=0;
+
+                if ("W" == StartingDirection.intern() || "E" == StartingDirection.intern())
+                {
+                    NumberOfRows = GalacticSize + 3;
+                    NumberOfColumns = GalacticSize + 2;
+                }
+                else if ("N" == StartingDirection.intern() || "S" == StartingDirection.intern())
+                {
+                    NumberOfRows = GalacticSize + 2;
+                    NumberOfColumns = GalacticSize + 3;
+                }
+                switch(StartingDirection)
+                {
+                    case "W": 
+                        HorizontalDirection = 1;
+                        HorizontalPointer = 0;
+                        VerticalPointer = 1;
+                        break;
+                    case "E":
+                        HorizontalDirection = -1;
+                        HorizontalPointer = NumberOfColumns - 1;
+                        VerticalPointer = NumberOfRows - 2;
+                        break;
+                    case "N":
+                        VerticalDirection = 1;
+                        HorizontalPointer = NumberOfColumns - 2;
+                        VerticalPointer = 0;
+                        break;
+                    case "S":
+                        VerticalDirection = -1;
+                        HorizontalPointer = 1;
+                        VerticalPointer = NumberOfRows - 1;
+                        break;
+                }
+
+                String[][] Tablica;
+                Tablica = new String[NumberOfRows][NumberOfColumns];
+
+                for (int i=0; i<GalacticSize-1; i++)
+                {
+                    SpiralLength = SpiralLength + i + 3;
+                }
+
+                int NumberOfDirectionChanges = 0;
+                int LengthToDirectionChange = GalacticSize + 1;
+                for (int i=0; i<SpiralLength ; i++)
+                {
+                    LengthToDirectionChange--;
+                    if (LengthToDirectionChange == 0)
+                    {
+                        NumberOfDirectionChanges++;
+                        LengthToDirectionChange = GalacticSize + 1 - NumberOfDirectionChanges;
+                        switch(CurrentDirection)
+                        {
+                            case "W": 
+                                HorizontalDirection = 0;
+                                VerticalDirection = 1;
+                                CurrentDirection = "N";
+                                break;
+                            case "E":
+                                HorizontalDirection = 0;
+                                VerticalDirection = -1;
+                                CurrentDirection = "S";
+                                break;
+                            case "N":
+                                VerticalDirection = 0;
+                                HorizontalDirection = -1;
+                                CurrentDirection = "E";
+                                break;
+                            case "S":
+                                VerticalDirection = 0;
+                                HorizontalDirection = 1;
+                                CurrentDirection = "W";
+                                break;
+                        }  
+                    }
+                    Tablica[VerticalPointer][HorizontalPointer] = "NCDC <3";
+                    VerticalPointer=VerticalPointer+VerticalDirection;
+                    HorizontalPointer=HorizontalPointer+HorizontalDirection;
+                }
+
+                //***************WYSWIETLANIE TABLICY************
+//                for (int i=0; i<NumberOfRows; i++)
+//                {
+//                    row = "";
+//                    for (int j=0; j<NumberOfColumns; j++)
+//                    {
+//                        if (Tablica[i][j]==null)
+//                        {
+//                            row=row+"*";
+//                        }
+//                        else
+//                        {
+//                            row=row+" ";
+//                        }
+//                    }
+//                    System.out.println(row);
+//                }
+                System.out.println(SpiralLength);
+
+            }
+            else
+            {
+                System.out.println("klops");
+            }
+        }
+        else
+        {
+            System.out.println("klops");
+        }
+    }  
+}
---
+++ Kosmolot.java
@@ -0,0 +1,109 @@
+package kosmolot;
+
+import java.lang.Math;
+
+/**
+ *
+ * 
+ * @author Jakub Rutkowski
+ */
+
+public class Kosmolot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer RocketSize = Integer.parseInt(args[0]);
+        String Armor = args[1];
+        boolean RocketSizeValid = true;
+        boolean RocketArmorValid = true;
+        boolean NumberOfArgumentsIsValid = true;
+        String row="";
+        
+        int NumberOfArguments = args.length;
+        if (NumberOfArguments != 2)
+        {
+            NumberOfArgumentsIsValid = false;
+        }
+        
+        //System.out.println("RocketSize = " + RocketSize + " Armor = " + Armor);
+        
+        if (RocketSize > 75 || RocketSize < 1) RocketSizeValid = false;
+        if ("Y" != Armor.intern() && "N" != Armor.intern()) RocketArmorValid = false;
+ 
+        if (RocketSizeValid == true && RocketArmorValid == true && NumberOfArgumentsIsValid == true)
+        {
+            Integer NumberOfRows = 2*RocketSize-1;
+            Integer NumberOfColumns = RocketSize*RocketSize;
+            String[][] Tablica;
+            Tablica = new String[NumberOfRows][NumberOfColumns];
+            Integer RocketPart = 0;
+        
+            for (int k=0;k<RocketSize;k++)
+            {
+                for (int i=0; i<RocketSize; i++)
+                {
+                    for (int j=0; j<RocketSize; j++)
+                    {
+                        if(i>=j)
+                        {
+                            Tablica[i][j+RocketPart] = "*";
+                            Tablica[NumberOfRows-1-i][j+RocketPart] = "*";
+                        }
+                    }
+
+                }
+                RocketPart=RocketPart+RocketSize;   
+            }
+
+            if ("Y" == Armor.intern())
+            {           
+                for (int i=0; i<NumberOfRows; i++)
+                {
+                    Tablica[i][0] = ">";
+                    for (int j=1; j<NumberOfColumns-1; j++)
+                    {
+                        if (Tablica[i][j]=="*" && Tablica[i][j+1]==null)
+                        {
+                            if (i>NumberOfRows/2)
+                            {
+                                Tablica[i][j]="/";
+                            }
+                            else
+                            {
+                                Tablica[i][j]="\\";
+                            }
+                        }
+                    }
+                }
+                Tablica[RocketSize-1][NumberOfColumns-1]=">";
+            }
+
+            //**************Wyswietlanie tablicy***********************
+            for(int i=0; i<NumberOfRows; i++)
+            {
+
+                row = "";
+                for(int j=0; j<NumberOfColumns; j++)
+                {
+                    if (Tablica[i][j]==null)
+                    {
+                        row=row+" ";
+                    }
+                    else
+                    {
+                    row=row+Tablica[i][j];
+                    }
+
+                }
+                System.out.println(row);
+            }
+        }
+        else
+        {
+            System.out.println("klops");
+        }
+    }
+    
+}
